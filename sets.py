926aa25baae0563d388ed471893b1c3d857d7904
zka-fdsfo <zaid292474@gmail.com>
2026-06-29 11:21:38 +0530

    set

---
+++ sets.py
@@ -0,0 +1,22 @@
+# myset = {"apple", "banana", "cherry", "apple", 2, 0, False}
+# #  unordered, unchangeable*, and unindexed.
+# print(len(myset))
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {1, 5, 7, 9, 3}
+# set3 = {True, False, False}
+
+# set1 = {"abc", 34, True, 40, "male"}
+# print(type(set1))
+
+
+# thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#     print(x)
+
+print("banana" not in thisset)
