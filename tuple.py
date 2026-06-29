d3b17b5d1d93cc07e58f79c80bb8256673a8a32d
zka-fdsfo <zaid292474@gmail.com>
2026-06-29 11:10:02 +0530

    newko

--- tuple.py
+++ tuple.py
@@ -29,3 +29,59 @@
 # print(thistuple[-4:-1])
 
 
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y.append("kiwi")
+# x = tuple(y)
+
+# print(x)
+
+
+# thistuple = ("apple", "banana", "cherry")
+# y = ("orange",)
+# thistuple += y
+
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# y.remove("apple")
+# thistuple = tuple(y)
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry")
+# del thistuple
+# print(thistuple) #this will raise an error because the tuple no longer exists
+
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "jamun")
+
+# (green, *yellow, red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in range(len(thistuple)):
+#   print(thistuple[x])
+
+# thistuple = ("apple", "banana", "cherry")
+# i = 0
+# while i < len(thistuple):
+#   print(thistuple[i])
+#   i = i + 1
+
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+# print(mytuple)
+
+
