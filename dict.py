558514dae26c9afde6389ccf831dfb8134b67b88
sawankumar543 <devnagricode@gmail.com>
2026-06-29 01:17:40 -0700

    add some code

--- dict.py
+++ dict.py
@@ -13,10 +13,24 @@
 # print(thisdict)
 # print(type(thisdict))
 
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for index, (key, value) in enumerate(thisdict.items()):
+#     print(index, key, value)
+
+
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-x = thisdict["model"]
-print(x)
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+# thisdict["year"] = 2018
+thisdict.update({"model": "red"})
+
+print(thisdict)
