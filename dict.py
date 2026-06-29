99825c05d33faae5a994a6a464e48fac9a66de1c
sawankumar543 <devnagricode@gmail.com>
2026-06-29 00:58:32 -0700

    dict file created
    Co-authored-by: zaid <zka-fdsfo@users.noreply.github.com>

---
+++ dict.py
@@ -0,0 +1,22 @@
+# thisdict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year": 1964
+# }
+# thisdict = {
+#     "brand":2,
+#     "model":3,
+#     "model":3,
+#     "year":5,
+#     "year":3
+# }
+# print(thisdict)
+# print(type(thisdict))
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
