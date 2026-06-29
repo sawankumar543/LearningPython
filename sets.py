c1e3a08456d883ac4747ec6fea2d2b89ae43c006
zka-fdsfo <zaid292474@gmail.com>
2026-06-29 11:35:17 +0530

    new set code

--- sets.py
+++ sets.py
@@ -14,9 +14,55 @@
 # print(thisset)
 
 
-thisset = {"apple", "banana", "cherry"}
+# thisset = {"apple", "banana", "cherry", True}
+
+# # for x in thisset:
+# #     print(x)]
+
+
+# print("banana" not in thisset)
+# print("banana" in thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.add("orange")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# tropical = ["pineapple", "mango", "papaya"]
+
+# thisset.update(tropical)
+# print(thisset)
+
+# thisset = {"apple", "cherry"}
+
+# # thisset.remove("banana")
+# # thisset.discard("banana")
+# # thisset.pop()
+# # thisset.clear()
+# del thisset
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
 
 # for x in thisset:
-#     print(x)
+#   print(x)
 
-print("banana" not in thisset)
+
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+# set3 = {"John", "Elena"}
+# set4 = {"apple", "bananas", "cherry"}
+
+# set3 = set1.union(set2, set3, set4)
+
+# print(set3)
+
+
+# x = {"a", "b", "c"}
+# y = (1, 2, 3)
+
+# z = x.union(y)
+# print(z)
