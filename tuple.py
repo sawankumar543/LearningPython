eea307beafdcde5bcc9e7b7ba49309c43a79b60e
sawankumar543 <devnagricode@gmail.com>
2026-06-28 22:48:27 -0700

    learning sets

--- tuple.py
+++ tuple.py
@@ -85,3 +85,8 @@
 # print(mytuple)
 
 
+# thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+thistuple = ("apple", "banana", "cherry ","apple")
+x = thistuple.count('a')
+y = thistuple.index("apple")
+print(y)
