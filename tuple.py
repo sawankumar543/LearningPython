2c5edc49212077584e12a40ccd98a41164b0aec3
zka-fdsfo <zaid292474@gmail.com>
2026-06-29 10:56:39 +0530

    new commmit

--- tuple.py
+++ tuple.py
@@ -8,6 +8,24 @@
 # print(len(fruits))
 # print(type(fruits))
 
-tuple1 = ("apple", "banana", "cherry")
-tuple2 = (1, 5, 7, 9, 3)
-tuple3 = (True, False, False)
+# tuple1 = ("apple", "banana", "cherry")
+# tuple2 = (1, 5, 7, 9, 3)
+# tuple3 = (True, False, False)
+
+
+# tuple1 = ("abc", 34, True, 40, "male")
+# print(tuple1)
+
+# thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-2])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+# print(thistuple[:5])
+# print(thistuple[4:])
+# print(thistuple[-4:-1])
+
+
