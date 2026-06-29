344473ed1dd6528b8a467672b89375d571dcf12e
zka-fdsfo <zaid292474@gmail.com>
2026-06-29 10:47:28 +0530

    new

--- tuple.py
+++ tuple.py
@@ -1,5 +1,13 @@
-mytuple = ("apple", "banana", "cherry")
-
+# mytuple = ("apple",)
+# fruits = ['apple']
 # A tuple is a collection which is ordered and unchangeable.
 
-print(mytuple)
+# print(len(mytuple))
+# print(type(mytuple))
+
+# print(len(fruits))
+# print(type(fruits))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
